77081c2d19c40df70c559940704e54fcfd1f6c9e
Gianluca Pengo <gianluca.pengo@gmail.com>
2015-01-03 20:28:46 +0100

    travis test

--- test/index.test.js
+++ test/index.test.js
@@ -4,14 +4,9 @@ var nodeBoxView = require('../lib'),
 	pdfDocPath = './test/docs/test.pdf',
 	pdfUrl = 'https://github.com/gian788/node-box-view/blob/master/test/doc/test.pdf';
 
-
-
 var apiKey = process.argv[2] || process.env.APIKEY;
 
-
 var boxView = new nodeBoxView(apiKey || process.env.APIKEY);
-
-
 
 describe('Testing upload', function () {
 	it('upload file from disk', function (done) {
@@ -29,10 +24,14 @@ describe('Testing upload', function () {
 		this.timeout(10000)
 		boxView.uploadFromUrl(pdfUrl, {}, function(err, boxFile){
 		    if(err) return done(err);
-		    
+
 		    boxFile.should.be.instanceof(Object);
 		    boxFile.type.should.be.equal('document');
 		    done();	    
 		});
 	});
+});
+
+
+describe('Testing get', function () {
 });
